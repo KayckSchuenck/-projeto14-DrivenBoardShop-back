8a75dd057cf06fda0254d743287001725dc85e82
KayckSchuenck <ycferreiras@gmail.com>
2022-07-07 21:15:51 -0300

    Tela login, cadastro e nav bar feitas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express,{json} from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import authRouter from './routes/authRoutes.js'
 dotenv.config()
 
 const server=express()
 server.use(cors())
 server.use(json())
 
+server.use(authRouter)
 
 server.listen(process.env.PORT)
---
+++ src/middlewares/userMiddleware.js
@@ -0,0 +1,24 @@
+import db from '../db.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export async function validateCheckout(req,res,next){
+    const {authorization}=req.headers
+    const token = authorization?.replace("Bearer ", "").trim()
+    if(!token) return res.status(401).send("Token inválido, faça login novamente")
+    const chaveSecreta = process.env.JWT_SECRET;
+    try{
+        const {userId} = jwt.verify(token, chaveSecreta);
+        const session = await db.collection("sessoes").findOne({ token })
+        if (!session) return res.status(401).send("Usuário não encontrado, faça login novamente")
+        const user = await db.collection("usuarios").findOne({ 
+            _id: userId
+        })
+        if(!user) return res.status(404).send("Usuário não encontrado, faça seu cadastro")
+        next()
+    } catch(e){
+        res.status(401).send("Token inválida ou expirada")
+    }
+   
+}
